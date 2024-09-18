c3b9706109c7373fabc633ff456f7601366e1d5d
joako-18 <joako1327@gmai.com>
2024-09-18 12:57:06 -0600

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavegacionComponent } from './component/navegacion/navegacion.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { HeaderComponent } from "./component/header/header.component";
+import { FooterComponent } from "./component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavegacionComponent, DashboardComponent, MatDialogModule],
+  imports: [RouterOutlet, NavegacionComponent, DashboardComponent, MatDialogModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
